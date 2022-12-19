725c69ad968fce44162e320c83654df0de186823
Bernardo Falé <mbfale@ua.pt>
2022-12-19 22:24:29 +0000

    updates and new configs

--- cm_test/main.py
+++ cm_test/main.py
@@ -1,6 +1,6 @@
 from network import LoRa
 import socket
-import time
+from utime import ticks_us, ticks_diff, sleep
 
 # Please pick the region that matches where you are using the device
 
@@ -8,8 +8,155 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 i = 0
+lora.bandwidth(LoRa.BW_125KHZ)
+lora.sf(12)
 while True:
-    s.send('Ping')
-    print('Ping {}'.format(i))
-    i= i+1
-    time.sleep(5)
+    #Data rate = 250 bit/s
+    if i <= 5:
+        t = ticks_us()
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 0')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 440 bit/s
+    elif i > 5 and i <= 10:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(11)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 1')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 980 bit/s
+    elif i > 10 and i <= 15:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(10)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 2')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 1760 bit/s
+    elif i > 15 and i <= 20:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(9)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 3')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 3125 bit/s
+    elif i > 20 and i <= 25:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(8)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 4')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 5470 bit/s
+    elif i > 25 and i <= 30:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(7)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 5')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    #data rate = 11000 bit/s
+    elif i > 30 and i <= 35:
+        t = ticks_us()
+        lora.bandwidth(LoRa.BW_250KHZ)
+        lora.sf(7)
+        # make the socket blocking
+        s.setblocking(True)
+        s.send('Ping DR 6')
+        s.setblocking(False)
+        # print time
+        delta = ticks_diff(ticks_us(), t)
+        print("Sent. TX Time =", delta/1000)
+        i = i + 1
+        # get any data received (if any...)
+        data = s.recv(64)
+        print("Received:", data)
+        # print stats of last packet
+        print("Stats:", lora.stats())
+        print("\n")
+        # wait
+        sleep(5)
+    else:
+        i = 0
+
+
--- node_b/main.py
+++ node_b/main.py
@@ -8,9 +8,44 @@ lora = LoRa(mode=LoRa.LORA, region=LoRa.EU868)
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 s.setblocking(False)
 i = 0
+lora.bandwidth(LoRa.BW_125KHZ)
+lora.sf(12)
+
 while True:
-    if s.recv(64) == b'Ping':
-        s.send('Pong' + str(i))
-        print('Pong {}'.format(i))
-        i = i+1
+    i = i + 1 if i > 5 else 1 
+    i = 0 if i > 35 else 0
+    if s.recv(64) == b'Ping DR 0':
+        i = i + 1
+        s.send('Pong DR 0')
+        print('Pong DR 0')
+    elif s.recv(64) == b'Ping DR 1' or (i > 5 and i <= 10):
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(11)
+        s.send('Pong DR 1')
+        print('Pong DR 1')
+    elif s.recv(64) == b'Ping DR 2' or (i > 10 and i <= 15):
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(10)
+        s.send('Pong DR 2')
+        print('Pong DR 2')
+    elif s.recv(64) == b'Ping DR 3' or (i > 15 and i <= 20):
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(9)
+        s.send('Pong DR 3')
+        print('Pong DR 3')
+    elif s.recv(64) == b'Ping DR 4' or (i > 20 and i <= 25):
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(8)
+        s.send('Pong DR 4')
+        print('Pong DR 4')
+    elif s.recv(64) == b'Ping DR 5' or (i > 25 and i <= 30):
+        lora.bandwidth(LoRa.BW_125KHZ)
+        lora.sf(7)
+        s.send('Pong DR 5')
+        print('Pong DR 5')
+    elif s.recv(64) == b'Ping DR 6' or (i > 30 and i <= 35):
+        lora.bandwidth(LoRa.BW_250KHZ)
+        lora.sf(7)
+        s.send('Pong DR 6')
+        print('Pong DR 6')      
     time.sleep(5)
